38ae00ace1db1286392fae9388331d7d8199b54d
Aram Miquel <aram@kitmanlabs.com>
2017-08-15 10:22:49 +0100

    First Commit

---
+++ Coordinator.swift
@@ -0,0 +1,52 @@
+//
+//  Coordinator.swift
+//
+//  Created by Aram Miquel Mateu on 15/08/2017.
+//
+
+import UIKit
+
+protocol Coordinator: class {
+  var nextCoordinator: Coordinator? { get set }
+  var navigationController: UINavigationController { get }
+  
+  // Will pop any presented VC, NavController, or view that the coordinator has presented
+  func popToRoot()
+  
+  // Gets the presented view controller (via push or modally)
+  var topViewController: UIViewController? { get }
+}
+
+extension Coordinator {
+  func popToRoot() {
+    nextCoordinator?.popToRoot()
+    
+    navigationController.presentedViewController?.dismiss(animated: false, completion: nil)
+    navigationController.popToRootViewController(animated: false)
+    
+    nextCoordinator = nil
+  }
+  
+  var topViewController: UIViewController? { get {
+    func getTop(navController: UINavigationController) -> UIViewController? {
+      if let innerNavController = navController.presentedViewController as? UINavigationController {
+        return getTop(navController: innerNavController)
+      } else if let innerNavController = navController.topViewController as? UINavigationController {
+        return getTop(navController: innerNavController)
+      } else if let topVC = navController.topViewController {
+        return topVC
+      } else if let topVC = navController.presentedViewController {
+        return topVC
+      } else {
+        return nil
+      }
+    }
+    
+    if let topVC = nextCoordinator?.topViewController {
+      return topVC
+    } else {
+      return getTop(navController: navigationController)
+    }
+    }
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# coordinator-protocol
+# Coordinator Protocol in Swift
+In the iOS world, specially since Swift protocols, the coordinator pattern has become more and more popular. 
+
+The Coordinator Pattern decouples totally the flow from the ViewControllers to a Coordinator. This coordinator handles the inputs and outputs of each ViewController, and knows how the app should react to user interaction within those ViewControllers. 
+
+## Why
+- Decouples flow from ViewControllers
+- More reusability for ViewControllers
+- More structured code
+- Easier to modify, update or delete flows
+- Deeplinks way (really) easier
+
+## Why not
+- StoryBoards and Coordinators doesn't do well
+
+## Example
+
+In the `AppDelegate` `didFinishLaunchingWithOptions`:
+
+```
+let navController = UINavigationController()
+appCoordinator = AppCoordinator(navigationController: navController)
+
+window!.rootViewController = navController
+window!.makeKeyAndVisible()
+
+DispatchQueue.main.async {
+  appCoordinator.start()
+}
+```
+
+
+
+
+
